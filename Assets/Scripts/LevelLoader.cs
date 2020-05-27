c751338064329a84df1b53160b80ff629922109f
Sam Carty <samcarty311@gmail.com>
2020-05-27 01:26:15 +0100

    added menu system

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public void LoadStartMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame() {
+        SceneManager.LoadScene("Level");
+    }
+    
+    public void LoadGameOver() {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+    
+}
